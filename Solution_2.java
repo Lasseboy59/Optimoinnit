fc6ebe5b851b27a6ba6e60cda892dfab497431ef
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-09-13 18:59:37 +0300

    testing and added Solution_2

---
+++ Solution_2.java
@@ -0,0 +1,18 @@
+package omatohjelmat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_2 {
+
+    // Complete the oddNumbers function below.
+    static List<Integer> oddNumbers(int l, int r) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
