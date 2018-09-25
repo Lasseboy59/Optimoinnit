bd26efd5ddf206f3f3cb18ca20b23a73609d85ac
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-09-25 19:25:15 +0300

    still trying ... H_Hourglass

--- H_HourglassSum2.java
+++ H_HourglassSum2.java
@@ -1,19 +1,23 @@
 package hackertest;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-public class H_HourglassSum {
+public class H_HourglassSum2 {
 
     // Complete the hourglassSum function below.
     static int hourglassSum(int[][] arr) {
 
-        printThe2Array(arr);
-
+//        printThe2Array(arr);
         System.out.println("");
         int i = 0, j = 0, counter = 0;
-        int sumOfGlass = 0;
-        int biggestsumOfGlass = 0;
+        int sumOfGlass = -100;
+        int biggestsumOfGlass = -100;
         List<Integer> myList = new ArrayList<Integer>();
 
         for (int rivi = 0; rivi < arr.length; rivi++) {
@@ -33,20 +37,28 @@ public class H_HourglassSum {
 
                 if (sarake == 3) {
 
-                    System.out.println("-------------");
+                    System.out.println("********************");
                     System.out.println("array: " + myList);
+
                     for (Integer num : myList) {
-                        System.out.print(num + " + ");
+                        if (num == 0) {
+                            System.out.print(" + " + num);
+                        } else {
+                            System.out.print("  " + num);
+                        }
                     }
+
+                    System.out.println("\n*******************");
                     System.out.println("");
 
                     for (Integer num : myList) {
                         sumOfGlass += num;
                     }
                     System.out.println("ind(3) + ind(5): " + myList.get(3) + " + " + myList.get(5) + " = " + (myList.get(3) + myList.get(5)));
-                    System.out.println("sum: " + sumOfGlass + " - " + ((myList.get(3) + myList.get(5))));
+                    System.out.println("SUMMA: " + sumOfGlass + " - " + ((myList.get(3) + myList.get(5))));
+
                     sumOfGlass = sumOfGlass - (myList.get(3) + myList.get(5));
-                    System.out.println("sum: " + sumOfGlass);
+                    System.out.println("SUMMA: " + sumOfGlass);
                     if (sumOfGlass > biggestsumOfGlass) {
                         biggestsumOfGlass = sumOfGlass;
                         sumOfGlass = 0;
@@ -57,12 +69,17 @@ public class H_HourglassSum {
                     System.out.println("sarake == 3 -- break");
                     sumOfGlass = 0;
                     break;
+
                 }
 
                 System.out.println("-------------");
                 System.out.println("array: " + myList);
                 for (Integer num : myList) {
-                    System.out.print(num + " + ");
+                    if (num == 0) {
+                        System.out.print(" + " + num);
+                    } else {
+                        System.out.print("  " + num);
+                    }
                 }
                 System.out.println("");
 
@@ -70,9 +87,9 @@ public class H_HourglassSum {
                     sumOfGlass += num;
                 }
                 System.out.println("ind(3) + ind(5): " + myList.get(3) + " + " + myList.get(5) + " = " + (myList.get(3) + myList.get(5)));
-                System.out.println("sum: " + sumOfGlass + " - " + ((myList.get(3) + myList.get(5))));
+                System.out.println("SUMMA: " + sumOfGlass + " - " + ((myList.get(3) + myList.get(5))));
                 sumOfGlass = sumOfGlass - (myList.get(3) + myList.get(5));
-                System.out.println("sum: " + sumOfGlass);
+                System.out.println("SUMMA: " + sumOfGlass);
                 if (sumOfGlass > biggestsumOfGlass) {
                     biggestsumOfGlass = sumOfGlass;
                     sumOfGlass = 0;
@@ -92,6 +109,8 @@ public class H_HourglassSum {
             }
         }
 
+        System.out.println("suurin: " + biggestsumOfGlass);
+
         return biggestsumOfGlass;
     }
 
@@ -101,22 +120,25 @@ public class H_HourglassSum {
         final String FNAME = "c:\\temp\\hourglassSum.log";
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FNAME));
 
-//        int[][] arr = new int[6][6];
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+//        Random r = new Random();
+//        int randint = Math.abs(r.nextInt()) % 11;
 //
-//        for (int i = 0; i < 6; i++) {
-//            String[] arrRowItems = scanner.nextLine().split(" ");
-//            scanner.ssarakeip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//        int[][] arr = {{1, 1, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0},
+//        {0, 9, 2, -4, -4, 0}, {0, 0, 0, -2, 0, 0}, {0, 0, -1, -2, -4, 0}};
 //
-//            for (int j = 0; j < 6; j++) {
-//                int arrItem = Integer.parseInt(arrRowItems[j]);
-//                arr[i][j] = arrItem;
-//            }
-//        }
-        Random r = new Random();
-        int randint = Math.abs(r.nextInt()) % 11;
-        
-        int[][] arr = { { 1, 1, 1, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, 
-        { 0, 9, 2, -4, -4, 0 }, { 0, 0, 0, -2, 0, 0 }, { 0, 0, -1, -2, -4, 0 }};
+//        int[][] arr2 = {{-9, -9, -9, 1, 1, 1}, {0, -9, 0, 4, 3, 2}, {-9, -9, -9, 1, 2, 3},
+//        {0, 0, 8, 6, 6, 0}, {0, 0, 0, -2, 0, 0}, {0, 0, 1, 2, 4, 0}};
 
 //        int[][] arr = new int[6][6];
 //        for (int i = 0; i < arr.length; i++) {
@@ -127,7 +149,6 @@ public class H_HourglassSum {
 //            }
 ////            System.out.println("");
 //        }
-
         int result = hourglassSum(arr);
 
         bufferedWriter.write(String.valueOf(result));
